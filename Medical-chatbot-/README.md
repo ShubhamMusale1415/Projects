45e24dc43fdf9f151a678746f6b3297212d4cbe5
shubham <musaleshubham12@gmail.com>
2026-04-06 01:42:55 +0530

    changes saved successfully

---
+++ Medical-chatbot-/README.md
@@ -0,0 +1,5 @@
+# Medical-chatbot
+
+#for installing all the packages i need to run requirement.txt file  below is prompt to run this file .. 
+
+pip install -r requirements.txt
---
+++ Medical-chatbot-/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup 
+
+setup(
+    name="medical_chatbot",
+version="0.1.0",
+author="Shubham Musale",
+author_email="musaleshubham1118@gmail.com",
+packages=find_packages(),
+install_requires=[]
+)
